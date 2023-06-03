42dacc8b33feb8543929ee3f074017aaaedd5e57
Rybeardawg1 <104077296+Rybeardawg1@users.noreply.github.com>
2023-06-03 12:15:52 -0400

    Routes Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
 import "./bootstrap.min.css";
+import React from "react";
+// import * as ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./Home";
 import Login from "./Login";
-
+import Signup from "./Signup";
+// import Threads from "./Threads";
+// import IndividualThread from "./individualthread";
+// import Guidelines from "./Guidelines";
+// import ProfileView from "./ProfileView";
+// import ProfileEdit from "./ProfileEdit";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/threads" element={<Threads data={isbn} />} />
-        <Route path="/individualthread" element={<Results data={isbn} />} />
+        {/* <Route path="/threads" element={<Threads data={threads} />} />
+          <Route path="/individualthread" element={<IndividualThread data={messages} />} />
+          <Route path="/guidelines" element={<Guidelines />} />
+          <Route path="/profileview" element={<ProfileView />} />
+          <Route path="/profileedit" element={<ProfileEdit />} /> */}
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
